c30350bcfb41bf175eae90b22a4d8b8443f37ef5
AbiyyuMufti <akang.mufti@gmail.com>
2021-03-01 10:46:58 +0100

    Parameter - handler

---
+++ source/ParameterHandler.h
@@ -0,0 +1,13 @@
+/*
+ * ParameterHandler.h
+ *
+ *  Created on: 25 Feb 2021
+ *      Author: abiyy
+ */
+
+#ifndef SOURCE_PARAMETERHANDLER_H_
+#define SOURCE_PARAMETERHANDLER_H_
+
+
+
+#endif /* SOURCE_PARAMETERHANDLER_H_ */
